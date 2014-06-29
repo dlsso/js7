60eb080900b26cbaa213201c959cab623016c452
dlsso <supertonesfan@hotmail.com>
2014-06-29 15:13:17 -0600

    Added metrics bar and scroll percent

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
 $(document).on('ready', function() {
-  
+
+  var metricsHidden = true
+
+	$("#show-hide").on('click', function() {
+		var btnHeight = $("#show-hide").height() + 20
+		if(metricsHidden){
+			$("#metrics-content").removeAttr('id')
+			$("#metrics").animate({height:"100%"});
+			$("#show-hide").html("Hide Metrics");
+			metricsHidden = false;
+		}
+		else{
+			console.log(btnHeight)
+			$("#metrics").animate({height:btnHeight});
+			$("#show-hide").html("Show Metrics");
+			metricsHidden = true;			
+		}
+
+
+	})
+
+
+	scrollPercent = ($(window).scrollTop()/($(document).height() - $(window).height()))*100;
+	
+	$(document).on( 'scroll', function(){
+    	scrollPercent = Math.round(($(window).scrollTop()/($(document).height() - $(window).height()))*100);
+    	$("#scroll-percent").html(scrollPercent + " %")
+	});
+
+	
+
 });
