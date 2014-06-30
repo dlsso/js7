6ea1de73eb70fd87ec49697ba0110e93ff198771
dlsso <supertonesfan@hotmail.com>
2014-06-29 18:21:24 -0600

    Added logic and display for section timers

--- main.js
+++ main.js
@@ -50,27 +50,98 @@ $(window).bind("load", function() {
     	$("#scroll-distance").html(scrollDistance + " pixels")
 	});
 
+
+	var time1=0
+	var time2=0
+	var time3=0
+	var time4=0
+	var time5=0
+	var time6=0
+	var time7=0
+
 	setInterval(function(){
 		if($(window).scrollTop()<1000 ){
-			console.log("section 1")
+			time1 = time1 + 1
+			if(time1%60<10){
+				seconds1 = "0" + time1%60
+			}
+			else{
+				seconds1 = time1%60
+			}
+
+			$("#section1").html("Section 1. " + Math.floor(time1/60)+":"+ seconds1)
 		}
+
 		else if($(window).scrollTop()>1000 && $(window).scrollTop()<2000){
-			console.log("section 2")
+			time2 = time2 + 1
+			if(time2%60<10){
+				seconds2 = "0" + time2%60
+			}
+			else{
+				seconds2 = time2%60
+			}
+
+			$("#section2").html("Section 2. " + Math.floor(time2/60)+":"+ seconds2)
 		}
+
 		else if($(window).scrollTop()>2000 && $(window).scrollTop()<3000){
-			console.log("section 3")
+			time3 = time3 + 1
+			if(time3%60<10){
+				seconds3 = "0" + time3%60
+			}
+			else{
+				seconds3 = time3%60
+			}
+
+			$("#section3").html("Section 3. " + Math.floor(time3/60)+":"+ seconds3)
 		}
+
 		else if($(window).scrollTop()>3000 && $(window).scrollTop()<4000){
-			console.log("section 4")
+			time4 = time4 + 1
+			if(time4%60<10){
+				seconds4 = "0" + time4%60
+			}
+			else{
+				seconds4 = time4%60
+			}
+
+			$("#section4").html("Section 4. " + Math.floor(time2/60)+":"+ seconds4)
 		}
+
 		else if($(window).scrollTop()>4000 && $(window).scrollTop()<5000){
-			console.log("section 5")
+			time5 = time5 + 1
+			if(time5%60<10){
+				seconds5 = "0" + time5%60
+			}
+			else{
+				seconds5 = time5%60
+			}
+
+			$("#section5").html("Section 5. " + Math.floor(time5/60)+":"+ seconds5)
 		}
+
 		else if($(window).scrollTop()>5000 && $(window).scrollTop()<6000){
-			console.log("section 6")
+			time6 = time6 + 1
+			if(time6%60<10){
+				seconds6 = "0" + time6%60
+			}
+			else{
+				seconds6 = time6%60
+			}
+
+			$("#section6").html("Section 6. " + Math.floor(time6/60)+":"+ seconds6)
 		}
+
 		else{
-			console.log("section 7")
+			time7 = time7 + 1
+			if(time7%60<10){
+				seconds7 = "0" + time7%60
+			}
+			else{
+				seconds7 = time7%60
+			}
+
+			$("#section7").html("Section 7. " + Math.floor(time7/60)+":"+ seconds7)
 		}
 
 	},1000);
